50e8e85edd9b9d9ec52da84c9f5571baca6b8615
Sasa <salepn955@gmail.com>
2020-09-09 19:06:31 +0200

    CORS fix

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const ready = ()=>{
 const getQuote = async() => {
     // Show Loader
     loading();
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    const apiUrl = 'https://cors-anywhere.herokuapp.com/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
